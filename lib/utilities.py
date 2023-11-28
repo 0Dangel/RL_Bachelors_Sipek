fd74eca7f6208eaaa20af9e9381606f77c67c1bd
Daniel Šípek <sipek@energycloud.cz>
2023-11-28 07:16:56 +0100

    Code cleanup - unification of function

---
+++ lib/utilities.py
@@ -0,0 +1,10 @@
+def getInverseAction(action):
+    if(action == 0):
+        return 1    
+    if(action == 1):
+        return 0    
+    if(action == 2):
+        return 3    
+    if(action == 3):
+        return 2
+    return 1000
